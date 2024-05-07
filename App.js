874763e970851d5e85b59abc1036cb25d6510b9a
bonsaibauer <mail@philippfeierabend.de>
2024-05-07 10:20:10 +0200

    Änderungen für WhatsApp BE Version 3 Data Model

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Navigator from './src/navigation';
 
-export default function App() {
+// App.js
+
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+import { withAuthenticator } from "aws-amplify-react-native";
+
+// Hier wurde die Consolen - Warnung ausgeblendet
+Amplify.configure({ ...awsconfig, Analytics: { disabled: true } });
+// Amplify.configure(awsconfig)
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Navigator />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +25,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'whitesmoke',
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App);
