ef71c6271f345408bde0305fa7c1dbd9cbc45f28
bonsaibauer <mail@philippfeierabend.de>
2024-05-07 10:00:49 +0200

    Änderungen für WhatsApp UI Version 6 Input Box

---
+++ src/screens/ChatScreen.js
@@ -0,0 +1,33 @@
+import { Platform, ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from 'react-native';
+import Message from '../components/Message';
+import InputBox from '../components/InputBox';
+
+import bg from '../../assets/images/BG.png';
+import messages from '../../assets/data/messages.json';
+
+const ChatScreen = () => {
+  return (
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.bg}>
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          style={styles.list}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  bg: {
+    flex: 1,
+  },
+  list: {
+    padding: 10,
+  },
+});
+
+export default ChatScreen;
